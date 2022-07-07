a70c39c78a89edb7e584df4098e1687344bca0c3
kbstn <konrad.bestian@umwelt.uni-giessen.de>
2022-07-07 11:13:59 +0200

    fixed issue added comments

--- shared_expenses_app.py
+++ shared_expenses_app.py
@@ -21,56 +21,69 @@ st.markdown("""---""")
 
 left_column, right_column = st.columns(2)
 
-num_ppl= left_column.slider('Number of people',min_value=2, max_value=20, value=7)
-total_days = right_column.number_input('insert total number of days',step=1,min_value=1,value=31)
+# create a slide to define how many people participated
+num_ppl= left_column.slider('Number of people',min_value=2, max_value=20, value=5)
 
+# define the timespan for the calculations
+total_days = right_column.number_input('insert total number of days',step=1,min_value=1,value=31)
 total_days = int(total_days)
 
-left_column.write(num_ppl)
-right_column.write(total_days)
-
+# create as many columns as there are people defined
 pplcols = st.columns(num_ppl)
-ppl=[]
 
-for i, x in enumerate(pplcols):
-    name_list=['Sophia','Joanna','Georg','Flo','Konrad','Ida','Nuri','Surname8', 'Surname9', 'Surname10', 'Surname11', 'Surname12', 'Surname13', 'Surname14', 'Surname15', 'Surname16', 'Surname17', 'Surname18', 'Surname19', 'Surname20']
-    
-    ppl.append(x.text_input(f"Person # {i+1}",name_list[i],key=i))
-
+# dicts to store inforamtion about payment and money spend
 days ={}
 spend={}
 
-for i,x in enumerate(pplcols):
-    days[ppl[i]]=x.slider('Days '+ppl[i]+' was present',min_value=1, max_value=total_days,key=i, value=total_days)
-    spend[ppl[i]]=x.number_input('money spend by '+ppl[i],step=1,key=i)
+# for each column created this way create a text input for names with default values
+#
+for i, x in enumerate(pplcols):
+    # up to 20 default names
+    name_list=['Sophia','Joanna','Georg','Flo','Konrad','Ida','Nuri','Surname8', 'Surname9', 'Surname10', 'Surname11', 'Surname12', 'Surname13', 'Surname14', 'Surname15', 'Surname16', 'Surname17', 'Surname18', 'Surname19', 'Surname20']
+    
+    # get name of the person
+    person= x.text_input(f"Person # {i+1}",name_list[i],key=i)
 
+    # days are the days each person was participating. it will start with the same number 
+    # as the total number of days defined beforehands
+    days[person]=x.number_input('Days '+person+' was present',step=1,min_value=1,value=total_days, key=i)
+
+    # input field for the money spend by the person
+    spend[person]=x.number_input('money spend by '+person,step=10,key=i)
+
+# calcualte sums for days and money spend
 total_ppl_days = sum(days.values())
 total_spend = sum(spend.values())
+
+# calculate average money spend per 'person day'
 money_per_day = total_spend / total_ppl_days
 
+# make a nice line
 st.markdown("""---""")
 
-# metrics
+# show the calculated metrics
 col1, col2, col3 = st.columns(3)
 col1.metric("Cost/day/person", str(round(money_per_day,2))+' €')
 col2.metric("Total money spend", str(total_spend)+' €')
 col3.metric("Sum of all days", str(total_ppl_days)+' Days')
 
+# make a nice line
 st.markdown("""---""")
 
+# dump the stats in a dataframe
 results = pd.DataFrame({'days':pd.Series(days),'spend':pd.Series(spend)})
-
+# calculate cost per person and the difference
 costs_person = round(results.days * money_per_day,2)
 results['costs_person'] = costs_person
 results['difference'] = results.costs_person - results.spend
 
-# getting money
+# decie who is getting money
 getting = results[results.difference < 0]
 
-# have to pay
+# and who have to pay
 haveto = results[results.difference >= 0]
 
-
+# show results in a text format thats easy to copy and paste
 left_column2, right_column2 = st.columns(2)
 
 left_column2.markdown('## People that get money back')
@@ -88,6 +101,8 @@ for person in haveto.index.values:
     right_column2.markdown("In that time {} **consumed stuff with a value of {} Euro and paid {} Euro**".format(person,costs_person[0],haveto.loc[person,'spend'],2))
     right_column2.markdown("{} purchased less than the avarage thus **{} will have to pay {} € to the others**".format(person,person,abs(round(haveto.loc[person,'difference'],2))))
 
-st.markdown("""---""")
+st.markdown("""---""")# make a nice line
+
+# also show the the dataframe
 st.write('Check the data here:')
 st.write(results)
